17c07be9e1076f3ef5a12536db94b17e5a97b396
Peter Aye <paye001@hammer.cs.ucr.edu>
2017-11-28 14:25:05 -0800

    added some stuff to the functions

---
+++ visitor.cpp
@@ -0,0 +1,33 @@
+#include "visitor.h"
+
+PrintVisitor::PrintVisitor() { 
+    output = "";
+}
+
+void PrintVisitor::rootNode() {
+
+}
+
+void PrintVisitor::sqrNode() {
+
+}
+
+void PrintVisitor::multNode() {
+
+}
+
+void PrintVisitor::subNode() {
+
+}
+
+void PrintVisitor::addNode() {
+
+}
+
+void opNode(Op* op) {
+
+}
+
+void execute() {
+
+}
